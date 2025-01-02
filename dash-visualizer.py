10f382327d9a5b4e9d67a699ba94d9fd29938f21
Abishek Bhatta <abishekbhatta@MacBookAir.lan>
2025-01-02 16:53:36 -0500

    adding comments

--- dash-visualizer.py
+++ dash-visualizer.py
@@ -2,13 +2,16 @@ from dash import Dash, html, dcc
 import plotly.express as px
 import pandas as pd
 
+
+#intializing Dash app
 app = Dash()
 
+#load in data
 df = pd.read_csv('processed-data.csv')
 fig = px.line(df, x="date", y="sales")
+
+#create visualization
 fig.update_layout(xaxis_title = 'transaction-date', yaxis_title= 'pink morsels\' sales')
-
-
 app.layout = html.Div(children=[
     dcc.Graph(
         id='line-graph',
@@ -16,6 +19,8 @@ app.layout = html.Div(children=[
         style={'height':'90vh'}
     )
 ])
-app.run()
+
+if __name__ == '__main__':
+    app.run_server()
 
 
