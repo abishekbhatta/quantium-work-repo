28f09d0dbc78b3f875822c20fa344adee0961bba
Abishek Bhatta <abishekbhatta@MacBookAir.lan>
2025-01-02 14:21:50 -0500

    data-processing
    pushing data-processing function and processed data file called processed-data.csv

---
+++ data/data-processing.py
@@ -0,0 +1,25 @@
+import csv
+
+#process each record of daily_sales_file into sales, date, region
+
+def processing(csv_file):
+
+    with open(csv_file, mode= 'r') as read_file, open('processed-data.csv', mode='a') as write_file:
+        csv_reader = csv.reader(read_file, delimiter=',')  
+        csv_writer = csv.writer(write_file, delimiter=',') 
+        next(csv_reader)
+
+        for row in csv_reader:
+            food = row[0]
+            if food == 'pink morsel':
+                sales = int(float(row[1].strip('$'))) * int(row[2])
+                date = row[3]
+                region = row[4]
+                csv_writer.writerow([sales, date, region])
+
+
+processing('daily_sales_data_1.csv')
+processing('daily_sales_data_2.csv')
+
+
+
