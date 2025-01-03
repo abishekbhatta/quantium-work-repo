bdc5343898a67aa3914e65b519898141c6dfb009
Abishek Bhatta <abishekbhatta@MacBookAir.lan>
2025-01-02 19:50:38 -0500

    intial prodcution

--- dash-visualizer.py
+++ dash-visualizer.py
@@ -1,4 +1,4 @@
-from dash import Dash, html, dcc
+from dash import Dash, html, dcc, Input, Output, callback
 import plotly.express as px
 import pandas as pd
 
@@ -8,17 +8,35 @@ app = Dash()
 
 #load in data
 df = pd.read_csv('processed-data.csv')
-fig = px.line(df, x="date", y="sales")
 
 #create visualization
-fig.update_layout(xaxis_title = 'transaction-date', yaxis_title= 'pink morsels\' sales')
-app.layout = html.Div(children=[
-    dcc.Graph(
-        id='line-graph',
-        figure=fig,
-        style={'height':'90vh'}
-    )
+app.layout = html.Div(id='wrapper', children=[
+    dcc.RadioItems(
+        id='radio-buttons',
+        options=['East', 'West', 'North', 'South', 'All'],
+        value = 'All',
+    ),
+
+    dcc.Graph(id='line-graph')
 ])
+
+#updates graphs based on the selection
+
+@callback(
+    Output(component_id='line-graph', component_property='figure'),
+    Input(component_id='radio-buttons', component_property='value')
+)
+def update_graph(input_value):
+    figure = px.line(df, x="date", y="sales")
+    
+    #checks the region and filters dataframe accordingly
+    if input_value != 'All':
+        filtered_df = df[df['region'] == input_value.lower()]
+        figure = px.line(filtered_df, x="date", y="sales")
+
+    figure.update_layout(xaxis_title='transaction-date', yaxis_title='sales of pink morsel')
+    return figure
+
 
 if __name__ == '__main__':
     app.run_server()
