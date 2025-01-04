240f6549b1f42f448a7793ac4dee1162477d9709
Abishek Bhatta <abishekbhatta@MacBookAir.lan>
2025-01-03 19:38:57 -0500

    tested visualizer

---
+++ data_processing.py
@@ -0,0 +1,32 @@
+import csv
+
+# Process each record in the provided CSV file and write specific data to a new file
+
+def processing(csv_file):
+    # Open the input file for reading and the output file for appending
+    with open(csv_file, mode= 'r') as read_file, open('processed-data.csv', mode='a') as write_file:
+
+        csv_reader = csv.reader(read_file, delimiter=',')  
+        csv_writer = csv.writer(write_file, delimiter=',') 
+
+        next(csv_reader) #skip the header row
+
+        # Iterate over each row in the input file
+        for row in csv_reader:
+            food = row[0]
+            if food == 'pink morsel': # Filter rows where the food item is 'pink morsel'
+
+                #calculate sales and extract date and region
+                sales = float(row[1].strip('$')) * int(row[2])
+                date = row[3]
+                region = row[4] 
+                # Write the processed data to the output file
+                csv_writer.writerow([sales, date, region])
+
+
+processing('data/daily_sales_data_0.csv')
+processing('data/daily_sales_data_1.csv')
+processing('data/daily_sales_data_2.csv')
+
+
+
---
+++ test_dash_visualizer.py
@@ -0,0 +1,18 @@
+from dash_visualizer import app
+
+
+#test whether header is present 
+def test_header_loads(dash_duo):
+    dash_duo.start_server(app)
+    dash_duo.wait_for_element("#header", timeout=10)
+    assert dash_duo.find_element("#header").text == 'Pink Morsel Sales - Line Graph'
+
+#test whether graph is present
+def test_visualiation_loads(dash_duo):
+    dash_duo.start_server(app)
+    dash_duo.wait_for_element("#line-graph", timeout=10)
+
+#test whether picker is present
+def test_picker_loads(dash_duo):
+    dash_duo.start_server(app)
+    dash_duo.wait_for_element("#radio-buttons", timeout=10)
